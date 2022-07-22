4f0b29a199667afc10facc864ad5234035c2d3cb
AntonVoronezh <pstut@mail.ru>
2022-07-22 17:29:13 +0300

    add scss

--- aaa/src/App.js
+++ aaa/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     iiiiiiii
+    <div className="ui-wrapper">
+      <header>header</header>
+
+      <div className="ui-content-wrapper">
+        <section className="ui-section">
+          <div className="ui-container">
+            <h1 className="ui-title-1">Hello world!</h1>
+          </div>
+        </section>
+      </div>
+
+      <footer>footer</footer>
     </div>
   );
 }
