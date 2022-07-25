7e59429e80917e6ca76eacdb8c445a32d456274f
AntonVoronezh <pstut@mail.ru>
2022-07-25 15:55:01 +0300

    add avatar

--- aaa/src/App.js
+++ aaa/src/App.js
@@ -1,14 +1,21 @@
+import React from "react";
+
 import "./App.css";
 import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
+import Avatar from "./components/UI/Avatar";
 
 function App() {
+  const handleOnClick = () => console.log("handleOnClick");
+  const handleHeaderOnClick = () => console.log("handleHeaderOnClick");
+
   return (
     <div className="ui-wrapper">
-      <Header />
+      <Header onClick={handleHeaderOnClick} />
       <div className="ui-content-wrapper">
         <section className="ui-section">
           <div className="ui-container">
+            <Avatar isSquare onClick={handleOnClick} />
             <h1 className="ui-title-1">Hello world!</h1>
           </div>
         </section>
--- aaa/src/components/header/Header.jsx
+++ aaa/src/components/header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import propTypes from "prop-types";
 
 import "./Header.scss";
+import Avatar from "../UI/Avatar";
 
-export const Header = () => {
+const Header = ({ onClick }) => {
   return (
     <header className="Header">
       <div className="ui-container">
         <div className="Header_wrapper">
           <span className="Header_logo">Cv builder</span>
           <div className="Header_menu">
-            <a className="ui-button isLink">Print</a>
+            <a className="ui-button isLink" onClick={onClick}>
+              Print
+            </a>
             <div className="ui-button isLink">Github</div>
           </div>
         </div>
@@ -17,3 +22,13 @@ export const Header = () => {
     </header>
   );
 };
+
+Header.propTypes = {
+  onClick: propTypes.func,
+};
+
+Header.defaultProps = {
+  onClick: () => {},
+};
+
+export { Header };
