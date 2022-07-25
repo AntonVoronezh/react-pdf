e3ca2cd712d550e92f0d3e1a5048d95964cbb8dd
AntonVoronezh <pstut@mail.ru>
2022-07-25 14:57:30 +0300

    add header

--- aaa/src/components/header/Header.jsx
+++ aaa/src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import "./Header.scss";
+
 export const Header = () => {
   return (
-    <header>
-      <div className="ui-container">Header</div>
+    <header className="Header">
+      <div className="ui-container">
+        <div className="Header_wrapper">
+          <span className="Header_logo">Cv builder</span>
+          <div className="Header_menu">
+            <a className="ui-button isLink">Print</a>
+            <div className="ui-button isLink">Github</div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
