24d6e6748e72f008608c2b5f5d90ffa347f672e0
AntonVoronezh <pstut@mail.ru>
2022-07-25 15:10:31 +0300

    add footer

--- aaa/src/components/footer/Footer.jsx
+++ aaa/src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import "./Footer.scss";
+
 export const Footer = () => {
-   return (
-     <footer>
-       <div className="ui-container">footer</div>
-     </footer>
-   );
-}
+  return (
+    <footer className="Footer">
+      <div className="ui-container">
+        <a href="#">footer</a>
+      </div>
+    </footer>
+  );
+};
